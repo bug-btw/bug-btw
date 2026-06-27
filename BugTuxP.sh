59e75383ac896bfacc7c743f5ae03e22dc396004
bugbtw <135244853+bugbtw@users.noreply.github.com>
2026-06-27 04:36:02 -0300

    Modify sysctl settings for improved system performance
    Updated various kernel and VM parameters for performance tuning.

--- BugTuxP.sh
+++ BugTuxP.sh
@@ -305,33 +305,48 @@ cpu_current_info() {
 
 sysctl_content() {
     cat << 'EOF'
-vm.swappiness = 180
-vm.vfs_cache_pressure = 50
-vm.dirty_ratio = 10
-vm.dirty_background_ratio = 5
+vm.swappiness = 200
+vm.vfs_cache_pressure = 40
+vm.dirty_ratio = 15
+vm.dirty_background_ratio = 3
+vm.dirty_writeback_centisecs = 1500
+vm.dirty_expire_centisecs = 3000
 vm.max_map_count = 2147483642
 vm.compaction_proactiveness = 0
 vm.page-cluster = 0
 vm.oom_dump_tasks = 0
+vm.stat_interval = 10
+vm.zone_reclaim_mode = 0
 kernel.sched_autogroup_enabled = 1
 kernel.sched_migration_cost_ns = 500000
 kernel.nmi_watchdog = 0
 kernel.unprivileged_userns_clone = 1
 kernel.numa_balancing = 0
 kernel.perf_event_paranoid = 1
+kernel.pid_max = 4194304
+kernel.timer_migration = 0
+kernel.kptr_restrict = 0
 net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.rmem_default = 1048576
+net.core.wmem_default = 1048576
+net.ipv4.tcp_rmem = 4096 1048576 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
 net.core.netdev_max_backlog = 16384
 net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_slow_start_after_idle = 0
+net.core.somaxconn = 8192
+net.ipv4.tcp_max_syn_backlog = 8192
 fs.inotify.max_user_watches = 524288
 fs.inotify.max_user_instances = 1024
 fs.file-max = 2097152
 EOF
 }
+
 
 status_sysctl() {
     content_matches "$SYSCTL_FILE" "$(sysctl_content)" && echo OK || echo NO
